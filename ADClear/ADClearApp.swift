7343f7d44ba89e5a451cf93ff0b0f10f420d8481
Micky 的 MackbookAir <>
2026-05-07 14:40:25 +0800

    first commit

---
+++ ADClear/ADClearApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADClearApp.swift
+//  ADClear
+//
+//  Created by Micky on 2026/5/7.
+//
+
+import SwiftUI
+
+@main
+struct ADClearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
